c69532b2a7a42aef6200aa6f24b911c056c81581
uni20151535 <quffl9612@unist.ac.kr>
2021-02-05 20:42:48 +0900

    feat: delete clone.py

--- src/CJH.py
+++ src/CJH.py
@@ -1,9 +1,4 @@
 print('cha jun hyung, 20151535')
 print('hello')
 print('I modified once more.')
-<<<<<<< HEAD
-
-print('conflict message')
-=======
 print('I modified second time.')
->>>>>>> master
